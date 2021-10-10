1b3aac54ca57ec125a28053e2e7c9e674049698e
Elisa Lambert <elisa.lambertrondouin@gmail.com>
2021-10-10 17:32:20 +0200

    commit

--- src/fr/algorithmie/SommeDeTableauxDiff.java
+++ src/fr/algorithmie/SommeDeTableauxDiff.java
@@ -6,7 +6,7 @@ public class SommeDeTableauxDiff {
 
 		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
 		int[] array2 = {-1, 12, 17, 14, 5, -9, 0, 18 } ;
-		int[] arraySomme = new int[array2.length];
+		int[] arraySomme = new int[array.length];
 
 		for (byte i = 0; i <= array2.length - 1; i++) {
 
@@ -14,6 +14,12 @@ public class SommeDeTableauxDiff {
 
 			System.out.println(arraySomme[i]);
 		}
-
+		for (int i = array2.length-1; i < arraySomme.length; i++) {
+			
+			arraySomme[i]= array[i];
+			System.out.println(arraySomme[i]);
+			
+		}
+		
 	}
 }
